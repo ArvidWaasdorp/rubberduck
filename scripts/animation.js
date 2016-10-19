7a065eb51d126c4ad064a5b88463317a56efee5e
Fay Gramberg <faygramberg@gmail.com>
2016-10-19 17:12:19 +0200

    added animation.js + started with creating animation

---
+++ scripts/animation.js
@@ -0,0 +1,11 @@
+
+// ANIMATION 
+
+$(document).ready(function(){
+        var div = $(".tagline");
+        div.animate({left: '200px'}, "slow");
+        div.animate({fontSize: '80px'}, "slow");
+        $(this).css("color","red");
+      	console.log("het werkt wel");
+});
+
